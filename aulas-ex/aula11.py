55799a23ae5353c114a85a9af1da66f5c514f2e7
Fit_lustre0 <139131187+Fitlustre@users.noreply.github.com>
2025-08-26 15:37:36 +0100

    Aula 11 info

---
+++ aulas-ex/aula11.py
@@ -0,0 +1,9 @@
+
+nome = 'Guanabara'
+cores = {
+         'limpa': '\033[m',
+         'azul': '\033[1;34m',
+         'amarelo': '\033[33m',
+         'pretoebranco': '\033[7m'
+        }
+print(f'Olá, muito prazer em te conhecer, {cores['amarelo']}{nome}{cores['limpa']}!')
