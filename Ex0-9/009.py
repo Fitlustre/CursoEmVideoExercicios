4b3da9053e1deda214d2fd83f24a8bb593e0bbaa
Fit_lustre0 <139131187+Fitlustre@users.noreply.github.com>
2025-09-08 16:57:41 +0100

    0-9

--- Ex0-9/009.py
+++ Ex0-9/009.py
@@ -1,6 +1,18 @@
 #Exercício Python 9: Faça um programa que leia um número Inteiro qualquer e mostre na tela a sua tabuada.
 
 n = int(input(f'Digite um número inteiro: '))
+print('\033[35m=_=' * 10)
 print(
-    f"{'=' * 20} \n {n} x1=  {n * 1} \n {n} x2=  {n * 2} \n {n} x3=  {n * 3} \n {n} x4=  {n * 4} \n {n} x5=  {n * 5} \n"
-    f" {n} x6=  {n * 6} \n {n} x7=  {n * 7} \n {n} x8=  {n * 8} \n {n} x9=  {n * 9} \n {n} x10= {n * 10} \n {'='*20} ")
+    f'\033[34m'
+    f" {n} x1=  {n * 1} \n"
+    f" {n} x2=  {n * 2} \n"
+    f" {n} x3=  {n * 3} \n"
+    f" {n} x4=  {n * 4} \n"
+    f" {n} x5=  {n * 5} \n"
+    f" {n} x6=  {n * 6} \n"
+    f" {n} x7=  {n * 7} \n"
+    f" {n} x8=  {n * 8} \n"
+    f" {n} x9=  {n * 9} \n"
+    f" {n} x10= {n * 10} \n"
+      )
+print('\033[35m=_=' * 10)
