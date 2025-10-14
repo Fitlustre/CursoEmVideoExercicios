2191bb99b220f7731d9f4112d53dd01a30fa5347
Fit_lustre0 <139131187+Fitlustre@users.noreply.github.com>
2025-10-14 09:43:44 +0100

    60-69

--- ex60-69/069.py
+++ ex60-69/069.py
@@ -1,1 +1,18 @@
 
+pmaior = h = women = 0
+while True:
+    id = int(input('Digite sua idade: '))
+    sex = input('Digite seu sexo[M/F]: ').lower[0]
+    if sex != 'm' or sex != 'f':
+        print('\033[31mDigite os dados correctamente!\033[38m')
+    else: 
+        if id > 18:
+            pmaior += 1
+        if sex == "m":
+            h += 1
+        if id < 20 and sex == "f":
+            women += 1
+    esc = input('Quer continuar?[S/N] ').lower[0]
+    if esc == 's':
+        break
+print(f'Ao todo existem {pmaior} pessoas maiores de idade. \nForam cadastrados {h} homens \nE {women} mulheres com menos de 20 anos.')
