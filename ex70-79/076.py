82ed1c34870eb73faed4b56b6fe6d5283e573b85
Fit_lustre0 <139131187+Fitlustre@users.noreply.github.com>
2025-10-27 16:53:53 +0000

    076
    Create a program to list products and prices in a table format.

---
+++ ex70-79/076.py
@@ -0,0 +1,18 @@
+# Crie um programa que tenha uma tupla única com nomes de produtos e seus respectivos preços, na sequência. No final,
+# mostre uma listagem de preços, organizando os dados em forma tabular.
+t = ('Lápis', 5.99,
+     'Caderno', 20.19,
+     'Estojo', 30.00,
+     'Caderno de Praia', 19.99,
+     'Mochila', 49.99
+     )
+print('-'*40)
+print(f'{'LOJA FIT':^40}')
+print('-'*40)
+for pos in range(len(t)):
+    if pos % 2 == 0:
+        tp = 30 - len(t[pos])
+        print(f'{t[pos]}{'.'*tp}R$ ', end='')
+    else:
+        print(t[pos])
+print('='*40)
