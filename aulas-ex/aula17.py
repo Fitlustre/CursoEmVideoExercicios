8f09c8740bb650fcd471d2e1eda86634b73b4a8e
Fit_lustre0 <luzipsepicgames@gmail.com>
2025-11-06 16:31:59 +0000

    aula17.py exercices

---
+++ aulas-ex/aula17.py
@@ -0,0 +1,7 @@
+lanche = ['Chocolate', 'Pipoca', 'Pizza', 'Gelado']
+
+# Para adicionar algo a lista:  lanche.insert(0, 'variavel') ou lanche.append('variavel')
+# Para apagar algo de uma lista: del lanche[3] ou lanche.pop() ou lanche.remove('variavel')
+# Valores em range valores = list(range(4, 11))
+# valores.sort()
+# valores.sort(reverse=True)
---
+++ ex70-79/078.py
@@ -0,0 +1,17 @@
+#
+valores = list()
+cont = 0
+for c in range(0, 5):
+    cont += 1
+    n = int(input(f'Digite um número [{cont}/5]: '))
+    valores.append(n)
+
+for c in valores
+
+print(
+    f'=_='*15,
+    f'\nVocê digitou os valores {valores}\n'
+    f'O maior valor foi {max(valores)} que está na {valores.index(max(valores))+1}ª posição.\n'
+    f'E o menor valor foi {min(valores)} que está na {valores.index(min(valores))+1}ª posição.'
+)
+
