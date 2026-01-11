1cde4ffdbf3c419bce5071aa3a953aafd80dbbb4
Fitlustre0 <luzipsepicgames@gmail.com>
2026-01-11 20:08:18 +0000

    ex 89fin

--- ex80-89/ex089.py
+++ ex80-89/ex089.py
@@ -8,8 +8,29 @@ while True:
 
     lista.append(l_temp[:])
     l_temp.clear()
+
     resp = input('Deseja continuar? [S/N]: ')[0]
     if resp in 'Nn':
+        print('=_='*20)
         break
 
 print(lista)
+print(
+    '\n'
+    f'Nº   NOME           MÉDIA\n',
+    '-'*30
+    )
+for n, c in enumerate(lista):
+    m = (c[1]+c[2])/2
+    print(f'{n:<5}{c[0]:<15}{m}')
+
+while True:
+    aluno = int(input('Mostrar notas de qual aluno? (999 para terminar): '))
+    print(len(lista))
+    if aluno < len(lista):
+        print(f'As notas de {lista[aluno][0]} são: {lista[aluno][1]}, {lista[aluno][2]}')
+    elif aluno == 999:
+        print('\033[31mFinalizando...\033[m')
+        break
+    else:
+        print('\033[31mValor não encontrado tente novamente!\033[m')
