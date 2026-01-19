09ddcc060e9d80d8cb3d3e85dfc1ea7f5bbfdfb8
Fitlustre0 <luzipsepicgames@gmail.com>
2026-01-19 12:00:19 +0000

    ex 90

--- Ex90-99/090.py
+++ Ex90-99/090.py
@@ -1,13 +1,15 @@
 
 alunos = {}
 alunos['nome'] = input('Nome: ')
-alunos['media'] = round(float(input(f'Média de {alunos["nome"]}: ')), 2)
+alunos['Média:'] = round(float(input(f'Média de {alunos["nome"]}: ')), 2)
 print('=_='*40)
-if 0 < alunos['media'] < 5:
-    print(f'{alunos[nome]} teve ma media de {alunos['media']} e está Reprovado')
-elif 5 <= alunos['media'] < 7:
-    print(f'{alunos[nome]} teve ma media de {alunos['media']} e está de Recuperação')
-elif 7 < alunos['media'] <= 10:
-    print(f'{alunos[nome]} teve ma media de {alunos['media']} e está Aprovado')
+if 0 < alunos['Média:'] < 5:
+    alunos['Situação:'] = '\033[31mReprovado!\033[m'
+elif 5 <= alunos['Média:'] < 7:
+    alunos['Situação:'] = '\033[32mRecuperação!\033[m'
+elif 7 < alunos['Média:'] <= 10:
+    alunos['Situação:'] = '\033[34mAprovado!\033[m'
 else:
     print('Erro de digitação!')
+    exit()
+print(f'{alunos['nome']} teve ma media de {alunos['Média:']} e está {alunos['Situação:']}')
--- Ex90-99/090a.py
+++ Ex90-99/090a.py
@@ -4,7 +4,7 @@ alunos['Média:'] = round(float(input(f'Média de {alunos["Nome:"]}: ')), 2)
 if 0 < alunos['Média:'] < 5:
     alunos['Situação:'] = '\033[31mReprovado!\033[m'
 elif 5 <= alunos['Média:'] < 7:
-    alunos['Situação:'] = '\033[m32Recuperação!\033[m'
+    alunos['Situação:'] = '\033[32mRecuperação!\033[m'
 elif 7 < alunos['Média:'] <= 10:
     alunos['Situação:'] = '\033[34mAprovado!\033[m'
 else:
